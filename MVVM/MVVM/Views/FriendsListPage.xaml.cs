d524cddf218618b4c89ded649262bfca9af5e982
Opilane TTHK <opilane@tthk.ee>
2021-01-27 10:44:54 +0200

    Add project files.

---
+++ MVVM/MVVM/Views/FriendsListPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static MVVM.ViewModel.FriendListViewModel;
+
+namespace MVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FriendsListPage : ContentPage
+    {
+        public FriendsListPage()
+        {
+            InitializeComponent();
+            BindingContext = new FriendsListViewModel() { Navigation = this.Navigation };
+        }
+    }
+}
